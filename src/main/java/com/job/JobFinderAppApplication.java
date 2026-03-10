bc938961ba3234baed40cc045750c3b35e475f8c
Shankar Singh <shankarrajputsingh.256@gmail.com>
2026-03-10 17:00:37 +0530

    initial commit

---
+++ src/main/java/com/job/JobFinderAppApplication.java
@@ -0,0 +1,13 @@
+package com.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFinderAppApplication.class, args);
+	}
+
+}
